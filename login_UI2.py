b9af0518fb63c56b27cea3649b4543fe8480211a
KarimFalciator <karimfalciator@gmail.com>
2024-09-21 15:35:40 +0100

    Turned Login screen into OOP code

--- login_UI2.py
+++ login_UI2.py
@@ -1,103 +1,85 @@
 import tkinter as tk
 from tkinter import ttk
 
-def login_UI():
+class login_UI:
 
-    login = tk.Tk()
-    login.geometry('275x450')
-    login.title('Login Page')
+    def __init__(self, login):
+        self.login = login
+        self.login.title('Login')
+        self.login.geometry('215x300')
+        self.login.resizable(False, False)
 
-    notebook = ttk.Notebook(login)
-    notebook.pack(pady=0, expand=True)
+        self.notebook = ttk.Notebook(login)
+        self.notebook.pack(pady=0, expand=True, fill='both')
+
+        self.create_customer_tab()
+        self.create_librarian_tab()
+        self.create_help_tab()
 
 #customer tab ---------------------------------------------------------------
 
-    customer_tab = ttk.Frame(notebook, width=300, height=490)
-    notebook.add(customer_tab, text='Customer Login')
-
-    customerID_label = ttk.Label(customer_tab, text = 'User ID')
-    customerID_label.pack(padx=10, pady=5)
-    customerID_entry = ttk.Entry(customer_tab)
-    customerID_entry.pack(padx=10, pady=5)
-
-    customerPass_label = ttk.Label(customer_tab, text = 'Password')
-    customerPass_label.pack(padx=10, pady=5)
-    customerPass_entry = ttk.Entry(customer_tab, show= '•')
-    customerPass_entry.pack(padx=10, pady=5)
-
-#show password button
-
-    def show_password(event):
-        customerPass_entry.config(show='')
+    def create_customer_tab(self):
+        customer_tab = ttk.Frame(self.notebook, width=300, height=490)
+        self.notebook.add(customer_tab, text='Customer Login')
         
-
-    def hide_password(event):
-        customerPass_entry.config(show='•')
-
-    show_pass_button = ttk.Button(customer_tab, text='👁', width=4)
-    show_pass_button.pack(padx=5, pady=5)
-    show_pass_button.bind('<ButtonPress-1>', show_password)
-    show_pass_button.bind('<ButtonRelease-1>', hide_password)
-
-#help frame
-
-    help_frame = ttk.Frame(customer_tab)
-    help_frame.pack(padx=10, pady=10, fill='both', expand=True)
-
-    ForgotPass_Button = ttk.Button(help_frame, text='Reset Password', width=15)
-    ForgotPass_Button.pack(padx=10, pady=5)
-    
-    NewAcc_Button = ttk.Button(help_frame, text='Create Account', width=15)
-    NewAcc_Button.pack(padx=10, pady=5)
+        self.customerID_label = ttk.Label(customer_tab, text='Customer ID')
+        self.customerID_label.pack(padx=10, pady=10)
+        self.customerID_entry = ttk.Entry(customer_tab)
+        self.customerID_entry.pack(padx=10, pady=5)
+        
+        self.customerPass_label = ttk.Label(customer_tab, text='Password')
+        self.customerPass_label.pack(padx=10, pady=5)
+        self.customerPass_entry = ttk.Entry(customer_tab, show='•')
+        self.customerPass_entry.pack(padx=10, pady=5)
+        
+        self.show_pass_button = ttk.Button(customer_tab, text='👁', width=4)
+        self.show_pass_button.pack(padx=5, pady=5)
+        
+        self.show_pass_button.bind('<ButtonPress>', self.show_password)
+        self.show_pass_button.bind('<ButtonRelease>', self.hide_password)
 
 #librarian tab --------------------------------------------------------------
 
-    librarian_tab = ttk.Frame(notebook, width=300, height=490)
-    notebook.add(librarian_tab, text='Librarian Login')
+    def create_librarian_tab(self):
+        librarian_tab = ttk.Frame(self.notebook, width=300, height=490)
+        self.notebook.add(librarian_tab, text='Librarian Login')
+        
+        self.librarianID_label = ttk.Label(librarian_tab, text='Librarian ID')
+        self.librarianID_label.pack(padx=10, pady=10)
+        self.librarianID_entry = ttk.Entry(librarian_tab)
+        self.librarianID_entry.pack(padx=10, pady=5)
+        
+        self.librarianPass_label = ttk.Label(librarian_tab, text='Password')
+        self.librarianPass_label.pack(padx=10, pady=5)
+        self.librarianPass_entry = ttk.Entry(librarian_tab, show='•')
+        self.librarianPass_entry.pack(padx=10, pady=5)
+        
+        self.show_pass_button = ttk.Button(librarian_tab, text='👁', width=4)
+        self.show_pass_button.pack(padx=5, pady=5)
+        
+        self.show_pass_button.bind('<ButtonPress>', self.show_password)
+        self.show_pass_button.bind('<ButtonRelease>', self.hide_password)
+        
+    def create_help_tab(self):
+        help_tab = ttk.Frame(self.notebook, width=300, height=490)
+        self.notebook.add(help_tab, text='Help')
+        
+        self.help_label = ttk.Label(help_tab, text='Unable to login?')
+        self.help_label.pack(padx=10, pady=10)
+        
+        self.C_Reset_button = ttk.Button(help_tab, text='User Reset Password')
+        self.C_Reset_button.pack(padx=10, pady=10)
 
-    librarianID_label = ttk.Label(librarian_tab, text = 'Librarian ID')
-    librarianID_label.pack(padx=10, pady=10)
-    librarianID_entry = ttk.Entry(librarian_tab)
-    librarianID_entry.pack(padx=10, pady=5)
-
-    librarianPass_label = ttk.Label(librarian_tab, text = 'Password')
-    librarianPass_label.pack(padx=10, pady=5)
-    librarianPass_entry = ttk.Entry(librarian_tab, show= '•')
-    librarianPass_entry.pack(padx=10, pady=5)
-
-#show password button
-
-    def show_password(event):
-        librarianPass_entry.config(show='')
-
-    def hide_password(event):
-        librarianPass_entry.config(show='•')
-
-    show_pass_button = ttk.Button(librarian_tab, text='👁', width=4)
-    show_pass_button.pack(padx=5, pady=5)
-    show_pass_button.bind('<ButtonPress-1>', show_password)
-    show_pass_button.bind('<ButtonRelease-1>', hide_password)
-
-#help frame
-
-    help_frame = ttk.Frame(librarian_tab)
-    help_frame.pack(padx=10, pady=10, fill='both', expand=True)
-
-    ForgotPass_Button = ttk.Button(help_frame, text='Reset Password', width=15)
-    ForgotPass_Button.pack(padx=10, pady=5)
+        self.L_Reset_button = ttk.Button(help_tab, text='Librarian Reset Password')
+        self.L_Reset_button.pack(padx=10, pady=10)
     
-    NewAcc_Button = ttk.Button(help_frame, text='Create Account', width=15)
-    NewAcc_Button.pack(padx=10, pady=5)
+    def show_password(self, event):
+        self.librarianPass_entry.config(show='')
+        
+    def hide_password(self, event):
+        self.librarianPass_entry.config(show='•')
 
 
-
-
-    notebook.pack(fill='both', expand=False)
-
-
-
-
-    tk.mainloop()
 
 
 
@@ -105,4 +87,6 @@ def login_UI():
 
 
 if __name__ == "__main__":  # for testing
-    login_UI()
+    login = tk.Tk()
+    log = login_UI(login)
+    login.mainloop()
