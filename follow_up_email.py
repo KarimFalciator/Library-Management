e4234d556cd6282d7d35ffbaee3cca2367aa692a
Karim Soliman <karimfalciator@gmail.com>
2024-11-28 15:57:12 +0000

    Update follow_up_email.py

--- follow_up_email.py
+++ follow_up_email.py
@@ -4,45 +4,37 @@ from email.message import EmailMessage
 
 class follow_email:
 
-    def __init__(self, t_id, s_id, s_email, ref):
+    def __init__(self, t_id, s_email, ref, r_type, r_description, r_date):
         self.conn = database.connect_to_db('lending.db')
         self.t_id = t_id
-        self.s_id = s_id
         self.s_email = s_email
         self.ref = ref
+        self.r_type = r_type
+        self.r_description = r_description
+        self.r_date = r_date
 
-    def submit_t_email(self):
-        s_email_check = database.check_s_email(self.conn, self.s_id, self.s_email)
+    def send_l_email(self):
         late_check = database.check_late(self.conn, self.ref) #have to create the function in database check if booked r_date is Null/None
-        
-
-
-
-
-        if s_email_check:
-            self.otp = self.generate_otp()
-            self.send_t_email(t_id, t_email)
-        else:
-            None
-
-    def send_t_email(self, s_id, receiver_email):
         sender_email = "karimfalciator@gmail.com"  # later change to the email of the library
         sender_password = "iveu rkbv tlwl edzc"  # later change to the password of the library
 
-        # Create the email message
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login(sender_email, sender_password)
+        if late_check: 
+            # Create the email message
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login(sender_email, sender_password)
 
-        msg = EmailMessage()
-        msg['Subject'] = 'Borrowed Resource Due Date'
-        msg['From'] = sender_email
-        msg['To'] = receiver_email
-        msg.set_content(f'Your OTP is {'otp'}')
+            msg = EmailMessage()
+            msg['Subject'] = 'Borrowed Resource Due Date'
+            msg['From'] = sender_email
+            msg['To'] = self.s_email
+            msg.set_content(f'Your {self.r_type, self.r_description} is due on {self.r_date}. Please return it on time to avoid any late fees.')
 
-        # Send the email
-        server.send_message(msg)
-        server.quit()
+            # Send the email
+            server.send_message(msg)
+            server.quit()
+        else:
+            None
 
     def follow_password(self):
         t_id = self.teacherID_entry.get()
