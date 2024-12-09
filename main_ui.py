cf950a6669b6e87b37e761f216999b2fdeab0e3b
Falciator <karimfalciator@gmail.com>
2024-12-09 14:16:01 +0000

    fixed previous borrowed tab

--- main_ui.py
+++ main_ui.py
@@ -37,6 +37,7 @@ class main_UI:
         self.create_settings_tab()
         self.create_help_tab()
         self.add_borrowed_from_db()
+        self.add_previous_from_db()
 
     # home tab ---------------------------------------------------------------
     
@@ -44,20 +45,20 @@ class main_UI:
         home_tab = ctk.CTkFrame(self.Notebook, width=300, height=490)
         self.Notebook.add(home_tab, text='Home')
 
-        columns = ('ref', 's_id', 'r_id', 'b_date', 'd_date', 'r_date')
-        self.current_tree = ttk.Treeview(home_tab, columns=columns, show='headings')
+        columns_home = ('ref', 's_id', 'r_id', 'b_date', 'd_date', 'r_date')
+        self.current_tree = ttk.Treeview(home_tab, columns=columns_home, show='headings')
 
-        bg_color = self.main._apply_appearance_mode(ctk.ThemeManager.theme["CTkFrame"]["fg_color"])
-        text_color = self.main._apply_appearance_mode(ctk.ThemeManager.theme["CTkLabel"]["text_color"])
-        selected_color = self.main._apply_appearance_mode(ctk.ThemeManager.theme["CTkButton"]["fg_color"])
+        bg_color_home = self.main._apply_appearance_mode(ctk.ThemeManager.theme["CTkFrame"]["fg_color"])
+        text_color_home = self.main._apply_appearance_mode(ctk.ThemeManager.theme["CTkLabel"]["text_color"])
+        selected_color_home = self.main._apply_appearance_mode(ctk.ThemeManager.theme["CTkButton"]["fg_color"])
 
         treestyle = ttk.Style()
         treestyle.theme_use('default')
-        treestyle.configure("Treeview", background=bg_color, foreground=text_color, fieldbackground=bg_color, borderwidth=0)
-        treestyle.map('Treeview', background=[('selected', bg_color)], foreground=[('selected', selected_color)])
+        treestyle.configure("Treeview", background=bg_color_home, foreground=text_color_home, fieldbackground=bg_color_home, borderwidth=0)
+        treestyle.map('Treeview', background=[('selected', bg_color_home)], foreground=[('selected', selected_color_home)])
         self.main.bind("<<TreeviewSelect>>", lambda event: self.main.focus_set())
 
-        self.current_tree.bind("<Button-3>", self.show_context_menu)
+        self.current_tree.bind("<Button-3>", self.show_context_menu_home)
 
         # Define headings
         self.current_tree.heading('ref', text='Reference')
@@ -78,9 +79,9 @@ class main_UI:
         self.current_tree.grid(row=0, column=0, columnspan=4, padx=10, pady=5, sticky='nsew')
 
         # Add a vertical scrollbar
-        scrollbar = ttk.Scrollbar(home_tab, orient='vertical', command=self.current_tree.yview)
-        scrollbar.grid(row=0, column=4, sticky='ns')
-        self.current_tree.configure(yscrollcommand=scrollbar.set)
+        scrollbar_home = ttk.Scrollbar(home_tab, orient='vertical', command=self.current_tree.yview)
+        scrollbar_home.grid(row=0, column=4, sticky='ns')
+        self.current_tree.configure(yscrollcommand=scrollbar_home.set)
 
         # Add Borrowed ResourceForm
         self.add_borrowed_CTklable = ctk.CTkLabel(home_tab, text="Add a new borrowed:")
@@ -104,30 +105,30 @@ class main_UI:
         self.add_borrowed = ctk.CTkButton(home_tab, text="Add Borrowed resource", command=self.add_borrowed_resource)
         self.add_borrowed.grid(row=4, column=1, columnspan=1, pady=10)
 
-    def show_context_menu(self, event):
+    def show_context_menu_home(self, event):
         # Get the selected item
         selected_item = event.widget.selection()
         if selected_item:
             # Create a context menu
-            context_menu = tk.Menu(event.widget, tearoff=0)
-            context_menu.add_command(label="Return Object", command= self.retun_object)
-            context_menu.add_command(label="Extend Due Date", command= self.extend_borrowed)
-            context_menu.add_command(label="close", command= self.close_context_menu)
+            context_menu_home = tk.Menu(event.widget, tearoff=0)
+            context_menu_home.add_command(label="Return Object", command= self.return_object)
+            context_menu_home.add_command(label="Extend Due Date", command= self.extend_borrowed)
+            context_menu_home.add_command(label="close", command= self.close_context_menu)
             
             # Show the context menu
-            context_menu.post(event.x_root, event.y_root)
+            context_menu_home.post(event.x_root, event.y_root)
 
              # Bind the focus out event to destroy the context menu
-            self.current_tree.bind("<FocusOut>", self.hide_context_menu)
+            self.current_tree.bind("<FocusOut>", self.hide_context_menu_home)
 
-    def hide_context_menu(self, event):
+    def hide_context_menu_home(self, event):
         if hasattr(self, 'context_menu'):
-            self.context_menu.unpost()
-            del self.context_menu
+            self.context_menu_home.unpost()
+            del self.context_menu_home
         else:
             pass
     
-    def retun_object(self):
+    def return_object(self):
         # Get the selected item
         selected_item = self.current_tree.selection()
         # Get the values of the selected item
@@ -227,19 +228,112 @@ class main_UI:
         self.Notebook.add(return_tab, text='Returned Borrowed')
 
         columns = ('ref', 's_id', 'r_id', 'b_date', 'd_date', 'r_date')
-        self.current_tree = ttk.Treeview(return_tab, columns=columns, show='headings')
+        self.returned_tree = ttk.Treeview(return_tab, columns=columns, show='headings')
 
-        bg_color = self.main._apply_appearance_mode(ctk.ThemeManager.theme["CTkFrame"]["fg_color"])
-        text_color = self.main._apply_appearance_mode(ctk.ThemeManager.theme["CTkLabel"]["text_color"])
-        selected_color = self.main._apply_appearance_mode(ctk.ThemeManager.theme["CTkButton"]["fg_color"])
+        bg_color_returned = self.main._apply_appearance_mode(ctk.ThemeManager.theme["CTkFrame"]["fg_color"])
+        text_color_returned = self.main._apply_appearance_mode(ctk.ThemeManager.theme["CTkLabel"]["text_color"])
+        selected_color_returned = self.main._apply_appearance_mode(ctk.ThemeManager.theme["CTkButton"]["fg_color"])
 
         treestyle = ttk.Style()
         treestyle.theme_use('default')
-        treestyle.configure("Treeview", background=bg_color, foreground=text_color, fieldbackground=bg_color, borderwidth=0)
-        treestyle.map('Treeview', background=[('selected', bg_color)], foreground=[('selected', selected_color)])
+        treestyle.configure("Treeview", background=bg_color_returned, foreground=text_color_returned, fieldbackground=bg_color_returned, borderwidth=0)
+        treestyle.map('Treeview', background=[('selected', bg_color_returned)], foreground=[('selected', selected_color_returned)])
         self.main.bind("<<TreeviewSelect>>", lambda event: self.main.focus_set())
 
-        self.current_tree.bind("<Button-3>", self.show_context_menu)
+        # self.returned_tree.bind("<Button-3>", self.show_context_menu_returned)
+
+        # Define headings
+        self.returned_tree.heading('ref', text='Reference')
+        self.returned_tree.heading('s_id', text='Student ID')
+        self.returned_tree.heading('r_id', text='Resource ID')
+        self.returned_tree.heading('b_date', text='Borrowed Date')
+        self.returned_tree.heading('d_date', text='Due Date')
+        self.returned_tree.heading('r_date', text='Returned Date')
+
+        # Define column widths
+        self.returned_tree.column('ref', width=80)
+        self.returned_tree.column('s_id', width=90)
+        self.returned_tree.column('r_id', width=90)
+        self.returned_tree.column('b_date', width=105)
+        self.returned_tree.column('d_date', width=90)
+        self.returned_tree.column('r_date', width=105)
+
+        self.returned_tree.grid(row=0, column=0, columnspan=4, padx=10, pady=5, sticky='nsew')
+
+        # Add a vertical scrollbar
+        scrollbar_returned = ttk.Scrollbar(return_tab, orient='vertical', command=self.returned_tree.yview)
+        scrollbar_returned.grid(row=0, column=4, sticky='ns')
+        self.returned_tree.configure(yscrollcommand=scrollbar_returned.set)
+
+    # def show_context_menu_returned(self, event):
+    #     # Get the selected item
+    #     selected_item = event.widget.selection()
+    #     if selected_item:
+    #         # Create a context menu
+    #         context_menu_returned = tk.Menu(event.widget, tearoff=0)
+    #         context_menu_returned.add_command(label="Return Object", command= self.return_object)
+    #         context_menu_returned.add_command(label="Extend Due Date", command= self.extend_borrowed)
+    #         context_menu_returned.add_command(label="close", command= self.close_context_menu)
+            
+    #         # Show the context menu
+    #         context_menu_returned.post(event.x_root, event.y_root)
+
+    #          # Bind the focus out event to destroy the context menu
+    #         self.returned_tree.bind("<FocusOut>", self.hide_context_menu_returned)
+
+    # def hide_context_menu_returned(self, event):
+    #     if hasattr(self, 'context_menu'):
+    #         self.context_menu_returned.unpost()
+    #         del self.context_menu_returned
+    #     else:
+    #         pass
+
+    def refresh_tree(self):
+        # Clear the Treeview
+        for item in self.current_tree.get_children():
+            self.current_tree.delete(item)
+
+        # Fetch all borrowed records from the database
+        borrowed_records = database.get_returned_borrowed(self.conn, self.t_id)
+
+        # Insert each borrowed record into the Treeview
+        for record in borrowed_records:
+            self.current_tree.insert('', 'end', values=record)
+
+    # def close_context_menu(self):
+    #     if hasattr(self, 'context_menu'):
+    #         self.context_menu.unpost()
+    #         del self.context_menu
+    #     else:
+    #         pass
+
+    def add_previous_from_db(self):
+        # Fetch all borrowed records from the database
+        returned_records = database.get_returned_borrowed(self.conn, self.t_id)
+
+        # Insert each borrowed record into the Treeview
+        for record in returned_records:
+            self.returned_tree.insert('', 'end', values=record)
+
+    # resource list tab --------------------------------------------------------------
+    def create_home_tab(self):
+        home_tab = ctk.CTkFrame(self.Notebook, width=300, height=490)
+        self.Notebook.add(home_tab, text='Home')
+
+        columns_home = ('ref', 's_id', 'r_id', 'b_date', 'd_date', 'r_date')
+        self.current_tree = ttk.Treeview(home_tab, columns=columns_home, show='headings')
+
+        bg_color_home = self.main._apply_appearance_mode(ctk.ThemeManager.theme["CTkFrame"]["fg_color"])
+        text_color_home = self.main._apply_appearance_mode(ctk.ThemeManager.theme["CTkLabel"]["text_color"])
+        selected_color_home = self.main._apply_appearance_mode(ctk.ThemeManager.theme["CTkButton"]["fg_color"])
+
+        treestyle = ttk.Style()
+        treestyle.theme_use('default')
+        treestyle.configure("Treeview", background=bg_color_home, foreground=text_color_home, fieldbackground=bg_color_home, borderwidth=0)
+        treestyle.map('Treeview', background=[('selected', bg_color_home)], foreground=[('selected', selected_color_home)])
+        self.main.bind("<<TreeviewSelect>>", lambda event: self.main.focus_set())
+
+        self.current_tree.bind("<Button-3>", self.show_context_menu_home)
 
         # Define headings
         self.current_tree.heading('ref', text='Reference')
@@ -260,56 +354,56 @@ class main_UI:
         self.current_tree.grid(row=0, column=0, columnspan=4, padx=10, pady=5, sticky='nsew')
 
         # Add a vertical scrollbar
-        scrollbar = ttk.Scrollbar(return_tab, orient='vertical', command=self.current_tree.yview)
-        scrollbar.grid(row=0, column=4, sticky='ns')
-        self.current_tree.configure(yscrollcommand=scrollbar.set)
+        scrollbar_home = ttk.Scrollbar(home_tab, orient='vertical', command=self.current_tree.yview)
+        scrollbar_home.grid(row=0, column=4, sticky='ns')
+        self.current_tree.configure(yscrollcommand=scrollbar_home.set)
 
         # Add Borrowed ResourceForm
-        self.add_borrowed_CTklable = ctk.CTkLabel(return_tab, text="Add a new borrowed:")
+        self.add_borrowed_CTklable = ctk.CTkLabel(home_tab, text="Add a new borrowed:")
         self.add_borrowed_CTklable.grid(row=1, column=0, padx=5, pady=0, sticky='n')
 
-        self.h_s_id_CTklabel = ctk.CTkLabel(return_tab, text="Student ID:")
+        self.h_s_id_CTklabel = ctk.CTkLabel(home_tab, text="Student ID:")
         self.h_s_id_CTklabel.grid(row=2, column=0, padx=5, pady=5)
-        self.h_s_id_entry = ctk.CTkEntry(return_tab)
+        self.h_s_id_entry = ctk.CTkEntry(home_tab)
         self.h_s_id_entry.grid(row=3, column=0, padx=5, pady=5)
 
-        self.h_r_id_CTklabel = ctk.CTkLabel(return_tab, text="Resource ID:")
+        self.h_r_id_CTklabel = ctk.CTkLabel(home_tab, text="Resource ID:")
         self.h_r_id_CTklabel.grid(row=2, column=1, padx=5, pady=5)
-        self.h_r_id_entry = ctk.CTkEntry(return_tab)
+        self.h_r_id_entry = ctk.CTkEntry(home_tab)
         self.h_r_id_entry.grid(row=3, column=1, padx=5, pady=5)
 
-        self.h_days_CTklabel = ctk.CTkLabel(return_tab, text="Days borrowed:")
+        self.h_days_CTklabel = ctk.CTkLabel(home_tab, text="Days borrowed:")
         self.h_days_CTklabel.grid(row=2, column=2, padx=5, pady=5)
-        self.h_days_entry = ctk.CTkEntry(return_tab)
+        self.h_days_entry = ctk.CTkEntry(home_tab)
         self.h_days_entry.grid(row=3, column=2, padx=5, pady=5)
 
-        self.add_borrowed = ctk.CTkButton(return_tab, text="Add Borrowed resource", command=self.add_borrowed_resource)
+        self.add_borrowed = ctk.CTkButton(home_tab, text="Add Borrowed resource", command=self.add_borrowed_resource)
         self.add_borrowed.grid(row=4, column=1, columnspan=1, pady=10)
 
-    def show_context_menu(self, event):
+    def show_context_menu_home(self, event):
         # Get the selected item
         selected_item = event.widget.selection()
         if selected_item:
             # Create a context menu
-            context_menu = tk.Menu(event.widget, tearoff=0)
-            context_menu.add_command(label="Return Object", command= self.retun_object)
-            context_menu.add_command(label="Extend Due Date", command= self.extend_borrowed)
-            context_menu.add_command(label="close", command= self.close_context_menu)
+            context_menu_home = tk.Menu(event.widget, tearoff=0)
+            context_menu_home.add_command(label="Return Object", command= self.return_object)
+            context_menu_home.add_command(label="Extend Due Date", command= self.extend_borrowed)
+            context_menu_home.add_command(label="close", command= self.close_context_menu)
             
             # Show the context menu
-            context_menu.post(event.x_root, event.y_root)
+            context_menu_home.post(event.x_root, event.y_root)
 
              # Bind the focus out event to destroy the context menu
-            self.current_tree.bind("<FocusOut>", self.hide_context_menu)
+            self.current_tree.bind("<FocusOut>", self.hide_context_menu_home)
 
-    def hide_context_menu(self, event):
+    def hide_context_menu_home(self, event):
         if hasattr(self, 'context_menu'):
-            self.context_menu.unpost()
-            del self.context_menu
+            self.context_menu_home.unpost()
+            del self.context_menu_home
         else:
             pass
     
-    def retun_object(self):
+    def return_object(self):
         # Get the selected item
         selected_item = self.current_tree.selection()
         # Get the values of the selected item
@@ -359,7 +453,7 @@ class main_UI:
             self.current_tree.delete(item)
 
         # Fetch all borrowed records from the database
-        borrowed_records = database.get_returned_borrowed(self.conn, self.t_id)
+        borrowed_records = database.get_all_borrowed(self.conn, self.t_id)
 
         # Insert each borrowed record into the Treeview
         for record in borrowed_records:
@@ -401,47 +495,6 @@ class main_UI:
         for record in borrowed_records:
             self.current_tree.insert('', 'end', values=record)
 
-    # resource list tab --------------------------------------------------------------
-    def create_resource_list_tab(self):
-        resource_list_tab = ctk.CTkFrame(self.Notebook, width=300, height=490)
-        self.Notebook.add(resource_list_tab, text='List of resources')
-
-        columns = ('Name', 'Author', 'Genre')
-        self.resource_list_tree = ttk.Treeview(resource_list_tab, columns=columns, show='headings')
-
-        # Define headings
-        self.resource_list_tree.heading('Name', text='Name of resource')
-        self.resource_list_tree.heading('Author', text='Author')
-        self.resource_list_tree.heading('Genre', text='Genre')
-
-        # Define column widths
-        self.resource_list_tree.column('Name', width=170)
-        self.resource_list_tree.column('Author', width=110)
-        self.resource_list_tree.column('Genre', width=110)
-
-        self.resource_list_tree.grid(row=0, column=0, columnspan=4, padx=10, pady=5)
-
-        # Add a vertical scrollbar
-        scrollbar = ttk.Scrollbar(resource_list_tab, orient='vertical', command=self.resource_list_tree.yview)
-        scrollbar.grid(row=0, column=4, sticky='ns')
-
-        self.resource_list_tree.configure(yscrollcommand=scrollbar.set)
-
-        # Add Resource Form
-        ctk.CTkLabel(resource_list_tab, text="resource Name:").grid(row=1, column=0, padx=5, pady=5)
-        self.resource_name = ctk.CTkEntry(resource_list_tab)
-        self.resource_name.grid(row=1, column=1, padx=5, pady=5)
-
-        ctk.CTkLabel(resource_list_tab, text="Author:").grid(row=2, column=0, padx=5, pady=5)
-        self.resource_author = ctk.CTkEntry(resource_list_tab)
-        self.resource_author.grid(row=2, column=1, padx=5, pady=5)
-
-        ctk.CTkLabel(resource_list_tab, text="Genre:").grid(row=3, column=0, padx=5, pady=5)
-        self.resource_genre = ctk.CTkEntry(resource_list_tab)
-        self.resource_genre.grid(row=3, column=1, padx=5, pady=5)
-
-        ctk.CTkButton(resource_list_tab, text="Add Resource", command=self.add_resource).grid(row=4, column=0, columnspan=2, pady=10)
-
     def add_resource(self):
         # Fetch details from entry widgets
         name = self.resource_name.get()
@@ -478,6 +531,5 @@ if __name__ == "__main__":  # for testing
 # fare in modo che la fuzione per aggiungere un borrowed al database chiami solo il borrowed non ritornati
 # fai in modo che si puo prendere uno solo di ogni oggetto
 # aggiungi oggetto nummero che vuole non perforza deve essere uno in piu se ce il oggetto
-# aggiungi t_id a risorse, cosi che un insegnante puo dare via solo i suoi oggetti non quelli di altri insegnanti
 # aggiungi una window per aggiungere nuovi oggetti se e la prima volta che si usa il programma, usa un text file per confermare se ha fatto log in (YES/NO)
 # aggiungi una window per aggiungere nuovi studenti se e la prima volta che si usa il programma, usa un text file per confermare se ha fatto log in (YES/NO)
